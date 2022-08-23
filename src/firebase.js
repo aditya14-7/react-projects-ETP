c7ad99206fdba785e2ec3fe0a4d9dd8ba803dc06
Aditya Pandey <adityapnd14@gmail.com>
2022-08-23 18:19:09 +0530

    firebase integrated in todo-app

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMe2ECe6kItmylXkKCb0tF9kkFW_NocoY",
+  authDomain: "todoapplicationetp.firebaseapp.com",
+  projectId: "todoapplicationetp",
+  storageBucket: "todoapplicationetp.appspot.com",
+  messagingSenderId: "141943975693",
+  appId: "1:141943975693:web:675d8a2c1c406ba58cca44"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
